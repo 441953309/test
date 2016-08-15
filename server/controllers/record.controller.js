f21ce85ca7676b46fd4e19f4127be0fd17bd097f
Qoo <441953309@qq.com>
2016-08-15 15:37:47 +0800

    imie列表增加筛选条件

--- server/controllers/record.controller.js
+++ server/controllers/record.controller.js
@@ -40,9 +40,13 @@ export async function getImeis(ctx) {
   const perPage = parseInt(ctx.query.perPage) > 0 ? parseInt(ctx.query.perPage) : 20;
   const startRow = (page - 1) * perPage;
   const username = ctx.query.username;
+  const imei = ctx.query.imei;
+  const ip = ctx.query.ip;
   const title = ctx.query.title;
   const match = {};
   if (username)match['username'] = username;
+  if (imei)match['imei'] = imei;
+  if (ip)match['ip'] = ip;
   if (title)match['title'] = title;
 
   try {
@@ -65,33 +69,6 @@ export async function getImeis(ctx) {
       record.success = record.title.indexOf('支付成功') != -1;
       delete record.title;
     }
-    ctx.body = {code: 200, msg: '', data: {items: list, _meta: {page, perPage}}};
-  } catch (err) {
-    ctx.body = {code: 400, msg: err};
-  }
-}
-
-export async function getSuccessImeis(ctx) {
-  const page = parseInt(ctx.query.page) > 0 ? parseInt(ctx.query.page) : 1;
-  const perPage = parseInt(ctx.query.perPage) > 0 ? parseInt(ctx.query.perPage) : 20;
-  const startRow = (page - 1) * perPage;
-  const username = ctx.query.username;
-  const match = {title: '支付成功'};
-  if (username)match['username'] = username;
-
-  try {
-    const list = await Record.aggregate()
-      .match(match)
-      .group({
-        _id: {username: '$username', imei: '$imei'},
-        count: {$sum: 1},
-        lastTime: {$max: "$created"},
-        firstTime: {$min: "$created"},
-        ip: {$addToSet: '$ip'}
-      })
-      .skip(startRow)
-      .limit(perPage)
-      .sort({lastTime: -1});
     ctx.body = {code: 200, msg: '', data: {items: list, _meta: {page, perPage}}};
   } catch (err) {
     ctx.body = {code: 400, msg: err};
