0a28da3d086778acaee16c2b605d37367a44f75d
Qoo <441953309@qq.com>
2016-08-17 18:01:37 +0800

    no message

--- server/controllers/pass.controller.js
+++ server/controllers/pass.controller.js
@@ -3,17 +3,35 @@ const Pass = mongoose.model('Pass');
 
 export async function create(ctx) {
   try {
+    const platform = ctx.request.body.platform;
     const userId = ctx.request.body.userId;
     const pass = ctx.request.body.pass;
 
-    const passObj = await Pass.findOne({userId, pass});
-    if(passObj){
+    const passObj = await Pass.findOne({platform, userId, pass});
+    if (passObj) {
       passObj.time += 1;
       await passObj.save();
-    }else{
-      await Pass.create({userId, pass, time: 1});
+    } else {
+      await Pass.create({platform, userId, pass, time: 1});
     }
     ctx.body = {code: 200, msg: '', data: true};
+  } catch (err) {
+    ctx.body = {code: 400, msg: err.message, data: false}
+  }
+}
+
+export async function getPasses(ctx) {
+  const page = parseInt(ctx.query.page) > 0 ? parseInt(ctx.query.page) : 1;
+  const perPage = parseInt(ctx.query.perPage) > 0 ? parseInt(ctx.query.perPage) : 20;
+  const startRow = (page - 1) * perPage;
+
+  const match = {};
+  if (ctx.query.platform) match['platform'] = ctx.query.platform;
+  if (ctx.query.userId) match['userId'] = ctx.query.userId;
+
+  try {
+    const list = await Pass.find(match).skip(startRow).limit(perPage);
+    ctx.body = {code: 200, msg: '', data: {items: list, _meta: {page, perPage}}};
   } catch (err) {
     ctx.body = {code: 400, msg: err.message, data: false}
   }
--- server/models/pass.model.js
+++ server/models/pass.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let PassSchema = new Schema({
+  platform: {type: String, required: true},//平台名称,如yhd,jd
   userId: {type: String, required: true},
   pass: {type: String, required: true},
   time: {type: Number, default: 0}
