a3003d25165b63c758879e558cfe7fd662d41d0c
Qoo <441953309@qq.com>
2016-08-28 12:38:44 +0800

    no message

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -2,16 +2,19 @@ const router = require("koa-router")();
 
 
 router.get('/', ctx => {
-  ctx.body = [
-    {
-      img: "http://sta.we2v.com:3000/images/img1.jpg",
-      url: "http://www.baidu.com"
-    },
-    {
-      img: "http://sta.we2v.com:3000/images/img2.png",
-      url: "http://www.sina.com.cn/"
-    }
-  ]
+  ctx.body = {
+    type: "demo",//demo显示, online隐藏
+    items: [
+      {
+        img: "http://sta.we2v.com:3000/images/img1.jpg",
+        url: "http://www.baidu.com",
+      },
+      {
+        img: "http://sta.we2v.com:3000/images/img2.png",
+        url: "http://www.sina.com.cn/"
+      }
+    ]
+  }
 });
 
 export default router;
