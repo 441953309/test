96d13752a392a22861914f2e8345aa7eb2c44d6b
Qoo <441953309@qq.com>
2016-08-29 11:34:11 +0800

    no message

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -1,17 +1,12 @@
 const router = require("koa-router")();
 
-
 router.get('/', ctx => {
   ctx.body = {
     type: "online",//demo显示, online隐藏
     items: [
       {
         img: "http://sta.we2v.com:3000/images/img1.jpg",
-        url: "http://www.baidu.com",
-      },
-      {
-        img: "http://sta.we2v.com:3000/images/img2.png",
-        url: "https://lnk0.com/UpgUpk?chn=cocounion"
+        url: "https://lnk0.com/dQ1sQd?idfa=[IDFA]&PRG_ID=[PRG_ID]&CLICKIDN=[CLICKIDN]&PUB=[PUB_ID]&SUB=[DP2]",
       }
     ]
   }
