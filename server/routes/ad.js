bbe96fdf38148f35b16fd18ce0261cd322224c05
Qoo <441953309@qq.com>
2016-08-29 11:40:42 +0800

    no message

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -5,7 +5,7 @@ router.get('/', ctx => {
     type: "online",//demo显示, online隐藏
     items: [
       {
-        img: "http://sta.we2v.com:3000/images/img1.jpg",
+        img: "http://sta.we2v.com/adimgs/img1.png",
         url: "https://lnk0.com/dQ1sQd?idfa=[IDFA]&PRG_ID=[PRG_ID]&CLICKIDN=[CLICKIDN]&PUB=[PUB_ID]&SUB=[DP2]",
       }
     ]
